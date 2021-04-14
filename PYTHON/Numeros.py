315f1cb21ab7ffb07a5f2e7d6660c35f2841ac71
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2021-04-14 20:23:47 -0300

    Agrego funciones con operaciones

---
+++ PYTHON/Numeros.py
@@ -0,0 +1,41 @@
+
+# Suma
+
+def suma (a = 0, b = 0):
+    print (a + b)
+
+suma (2, 3)
+suma (4, 1)
+suma ( 8 )
+
+print ('--------------')
+
+# Resta
+
+def resta(a, b):
+    print (a - b)
+
+resta (10, 8)
+
+print ('---------------')
+
+# Multiplicación 
+
+def multiplicacion (a, b):
+    print (a * b)
+
+multiplicacion (2, 5)
+multiplicacion (5, 10)
+multiplicacion (3, 9)
+
+print ('------------')
+
+# División 
+
+def division (a, b):
+    print (a // b)
+
+division (10, 5)
+division (20, 2)
+division (100, 25)
+
