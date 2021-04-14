dc6a644adf5a6cd577c8b2ef4c0b81f2d9db5046
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2021-04-14 20:05:28 -0300

    Agrega funciones con parámetros

--- PYTHON/Funciones-2.py
+++ PYTHON/Funciones-2.py
@@ -1,6 +1,6 @@
-def informacion(nombre, apodo):
+def informacion(nombre, apodo = 'Desconocido'):
     print(f"Soy {nombre} y me dicen {apodo}")
 
 informacion('Alan', 'cajeto')
 informacion('Julieta', 'cajeta')
-informacion('Cajeta', 'culo')
+informacion('Cajeta')
