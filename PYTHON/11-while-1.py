61a18cdf4a6b736c9a1c2bd905b5e8761578729a
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2021-04-16 20:34:08 -0300

    Explicación de while

---
+++ PYTHON/11-while-1.py
@@ -0,0 +1,18 @@
+numero = 0
+
+'''
+
+while numero <= 10:
+       print (numero)
+       numero += 1 # Incremento para evitar un Loop infinito
+
+'''
+
+# If  dentro del while
+
+while numero <= 10:
+    if numero == 5:
+        print ('CINCOOOOOOO!!')
+    else:
+        print (numero)
+    numero += 1
