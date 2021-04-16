cae06981e2f21489566b4e06e6ce082cd5038093
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2021-04-15 21:46:55 -0300

    Explicación de diccionario

---
+++ PYTHON/09-Dics-2.py
@@ -0,0 +1,54 @@
+# Iniciar un diccionario vacío
+
+jugador = {}
+''' print (jugador) '''
+
+# Se une un jugador
+
+
+
+jugador ['nombre'] = 'Alan'
+jugador ['puntaje'] = 0
+''' print(jugador) '''
+
+
+# Incrementando el puntaje
+
+''' 
+
+jugador ['puntaje'] = 100
+print (jugador)
+
+# Incrementando el puntaje
+
+jugador['puntaje'] = 200
+print (jugador)
+
+
+
+# Acceder a un valor
+
+ print (jugador.get('consola', 'No existe ese valor')) 
+ 
+'''
+
+
+# Iterar en el diccionario
+
+'''
+
+for llave, valor in jugador.items():
+    print (llave)
+    print (valor)
+
+'''
+
+# Eliminar jugador y puntaje
+
+'''
+
+del jugador ['nombre']
+del jugador ['puntaje']
+print (jugador) 
+
+'''
