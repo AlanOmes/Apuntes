0d8c73d64993f3dd34791eb8176ea4626afe537a
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2021-04-15 21:07:11 -0300

    Update 07-Condicionales-2.py

--- PYTHON/07-Condicionales-2.py
+++ PYTHON/07-Condicionales-2.py
@@ -6,5 +6,7 @@ if ocupacion == 'Estudiante':
     print('Tienes 50% de descuento')
 elif ocupacion == 'Jubilado':
     print('Tienes 75% de descuento')
+elif ocupacion == 'Desempleado':
+    print('Tienes un 10% de descuento')
 else:
     print ('Debes pagar el 100%')
