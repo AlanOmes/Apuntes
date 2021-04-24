aa519968c67020a8331b0901de204abf053337b0
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2021-04-24 01:14:18 -0300

    Hace mas eficientes las preguntas del examen

--- PYTHON/10-input-1.py
+++ PYTHON/10-input-1.py
@@ -42,16 +42,20 @@ nombre = input ('Ingrese su nombre: ')
 print (f'Hola, {nombre}, las siguientes preguntas se deben responder por SI o por NO: ')
 
 
-pregunta_1 = input ('Chuty es el mejor freestyler del mundo? \r\n ')
-correcta = 'SI'
-incorrecta = 'NO' 
-puntaje = 0
+# pregunta_1 = input ('Chuty es el mejor freestyler del mundo? \r\n ')
+puntaje = 0 # SE INCREMENTA EN 1 CUANDO LA RESPUESTA ES CORRECTA (por pregunta)
+
+'''
+
 
 if pregunta_1 == correcta:
+    puntaje = puntaje + 1 
     print ('Respuesta correcta')
 elif pregunta_1 == 'si':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_1 == 'Si':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_1 == incorrecta:
     print ('Respuesta incorrecta')
@@ -67,11 +71,16 @@ pregunta_2 = input ('Bnet es mejor que Gazir? \r\n')
 correcta = 'NO'
 incorrecta = 'SI'
 
+creo que lo intente con or igual y no funciono 
+
 if pregunta_2 == correcta:
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_2 == 'No':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_2 == 'no':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_2 == incorrecta:
     print ('Respuesta incorrecta')
@@ -87,11 +96,14 @@ pregunta_3 = input ('Juli está extremadamente buena? \r\n')
 correcta = 'SI'
 incorrecta = 'NO'
 
-if pregunta_3 == correcta:
+if pregunta_3 == 'SI':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_3 == 'Si':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_3 == 'si':
+    puntaje = puntaje + 1
     print ('Respuesta correcta')
 elif pregunta_3 == incorrecta:
     print ('Le recomendamos comprarse anteojos')
@@ -101,4 +113,27 @@ elif pregunta_3 == 'no':
     print ('Le recomendamos comprarse anteojos')
 else:
     print ('Por favor, solo responda con "SI o "NO"')
+
+if puntaje == 3:
+    ('Felicitaciones, aprobó el exámen')
+elif puntaje == 2:
+    ('Felicitaciones, aprobó el exámen')
+else:
+    print ('Retirate burro de mierda, puto')
+
+'''
+
+pregunta_4 = input ('Juli le tiene unas ganas inexplicables a Alan? \r\n')
+
+
+if pregunta_4 == 'Si' or pregunta_4 ==' SI' or pregunta_4 == 'si':
+    puntaje = puntaje + 1
+    print ('Respuesta correcta')
+else:
+    print ('Mirá si no le va a tener ganas...')
+
+if puntaje == 1:
+    print (f'Felicitaciones, aprobó el exámen, {nombre}' )
+else: 
+    print ('Puto') 
 
