8ccfd57bca51079f7899f802b3f22093549c6cb5
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2022-05-05 16:57:51 -0300

    decoradores

---
+++ Curso/04-Teoria-avanzada/02-Decoradores.py
@@ -0,0 +1,58 @@
+# Decoradores
+
+'''
+
+def decorar_arbol(arbolito):
+    
+    def luces():
+        print('*Agregando luces*')
+
+    def estrellita():
+        print('*Agregando estrellita*')
+
+    luces()
+    estrellita()
+    return arbolito
+
+@decorar_arbol # Captura la funcion de abajo
+def arbolito():
+    print('Soy un arbolito :)')
+
+arbolito()
+
+'''
+
+
+class A:
+    
+    def __init__(self, nombre):
+        self.__nombre = nombre
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, argumento):
+        print('*Modificando el atributo*')
+        self.__nombre = argumento
+
+    @nombre.getter
+    def nombre(self):
+        print('*Devolviendo el atributo*')
+        return self.__nombre
+
+    @nombre.deleter
+    def nombre(self):
+        print('*Eliminando el atributo*')
+        del self.__nombre
+
+a = A('Alan')
+print(a.nombre)
+
+a.nombre = 'Julieta'
+print(a.nombre)
+
+del a.nombre
+
+
